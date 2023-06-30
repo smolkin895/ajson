076d256884b7bc3a0ffab5c361cf2b213b5c0d87
asmolkin <asmolkin@ozon.ru>
2023-06-30 18:43:42 +0300

    scriv

---
+++ advanced_json/extended_decoder.py
@@ -0,0 +1,16 @@
+import json
+
+
+class ExtendedDecoder(json.JSONDecoder):
+    def __init__(self, **kwargs):
+        kwargs["object_hook"] = self.object_hook
+        super().__init__(**kwargs)
+
+    def object_hook(self, obj):
+        try:
+            name = obj["__extended_json_type__"]
+            decoder = getattr(self, f"decode_{name}")
+        except (KeyError, AttributeError):
+            return obj
+        else:
+            return decoder(obj)
