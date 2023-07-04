d830d02b19b219d4c65e0b0f569b5ed866d02da2
smolkin895 <smol140791@ya.ru>
2023-07-04 21:36:16 +0300

    add counter

--- advanced_json/extended_decoder.py
+++ advanced_json/extended_decoder.py
@@ -5,12 +5,14 @@ from collections import namedtuple
 class ExtendedDecoder(json.JSONDecoder):
     def __init__(self, **kwargs):
         kwargs["object_hook"] = self.object_hook
+        self.sucess_count = 0
         super().__init__(**kwargs)
 
     def object_hook(self, obj):
         try:
             name = obj["__extended_json_type__"]
             decoder = getattr(self, f"decode_{name}")
+            self.sucess_count += 1
         except (KeyError, AttributeError):
             return obj
         else:
